242f766f208558072da0c50159ad4598b22f9dd9
vaknin <pinkxalmadius@gmail.com>
2021-03-21 05:12:29 +0200

    implemented queue/dequeue push/pop.
    manip needs implement in both stack and queue

--- src/part3/queue.ts
+++ src/part3/queue.ts
@@ -1,9 +1,14 @@
 import { State, bind } from "./state";
-
+import * as R from "ramda";
+import { makeFailure } from "../lib/result";
 export type Queue = number[];
 
-export const enqueue = undefined;
-
+export const enqueue = <Queue, A>(x:number):State<Queue,A> =>{
+    return bind((s:Queue)=>[s,"enq"], ((b:string)=>{   
+        b === "enq" ?
+        (s:number[])=>([x].concat(s)) : makeFailure("Parameter Error"); ///need to check what the fuck is the problem
+    }))
+};
 export const dequeue = undefined;
 
 export const queueManip = undefined;
--- src/part3/stack.ts
+++ src/part3/stack.ts
@@ -1,9 +1,18 @@
 import { State, bind } from "./state";
-
+import { makeFailure, makeOk, Result } from "../lib/result";
 export type Stack = number[];
 
-export const push = undefined;
-
-export const pop = undefined;
-
+export const push = (x:number):State<Stack,Result<any>> =>{
+    return (s:Stack):[Stack,Result<any>]=>{   
+        return [[x].concat(s), makeOk(undefined)];
+    }
+};
+export const pop = (s:Stack):[Stack,Result<any>]=>{   
+    if(s.length > 0) {
+        return ([s.slice(1), makeOk(s[0])]);
+    } 
+    else{
+        return [s,makeFailure("Stack is already empty, Cannot Pop")];
+    }
+};
 export const stackManip = undefined;
